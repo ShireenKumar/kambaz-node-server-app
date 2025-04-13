6b738f3472eaef9ac056fdbd415ada0971f93720
ShireenKumar <shireen.kumar1304@gmail.com>
2025-04-13 04:05:58 -0400

    courses actually work noww

--- Kambaz/Database/enrollments.js
+++ Kambaz/Database/enrollments.js
@@ -1,6 +1,14 @@
 export default [
-    { "_id": "1", "user": "123", "course": "RS101" },  { "_id": "2", "user": "234", "course": "RS101" },
-    { "_id": "3", "user": "345", "course": "RS101" },  { "_id": "4", "user": "456", "course": "RS101" },
-    { "_id": "5", "user": "567", "course": "RS101" },  { "_id": "6", "user": "234", "course": "RS102" },
-    { "_id": "7", "user": "789", "course": "RS102" },  { "_id": "8", "user": "890", "course": "RS102" },
-    { "_id": "9", "user": "123", "course": "RS102" } ]
+    { "_id": "1", "user": "u1", "course": "RS101" },  { "_id": "2", "user": "u2", "course": "RS101" },
+    { "_id": "3", "user": "u3", "course": "RS101" },  { "_id": "4", "user": "u4", "course": "RS101" },
+    { "_id": "5", "user": "u5", "course": "RS101" },  { "_id": "6", "user": "u6", "course": "RS102" },
+    { "_id": "7", "user": "u7", "course": "RS102" },  { "_id": "8", "user": "u8", "course": "RS102" },
+
+    { "_id": "9", "user": "u9", "course": "RS102" },  { "_id": "10", "user": "u1", "course": "RS101" },
+    { "_id": "10", "user": "u1", "course": "RS102" }, { "_id": "10", "user": "u1", "course": "RS103" },
+    { "_id": "10", "user": "u1", "course": "RS104" },
+
+    { "_id": "11", "user": "u2", "course": "RS104"}, { "_id": "11", "user": "u2", "course": "RS103"}, 
+    { "_id": "11", "user": "u2", "course": "RS102"}
+
+]
