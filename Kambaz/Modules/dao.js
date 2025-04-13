a05fcdc610ca0277e3ac342b50409101b3be1e99
ShireenKumar <shireen.kumar1304@gmail.com>
2025-04-13 19:24:18 -0400

    Edit assignments and modules

--- Kambaz/Modules/dao.js
+++ Kambaz/Modules/dao.js
@@ -1,5 +1,6 @@
 import Database from "../Database/index.js";
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid';
+
 export function findModulesForCourse(courseId) {
   const { modules } = Database;
   return modules.filter((module) => module.course === courseId);
@@ -9,13 +10,11 @@ export function createModule(module) {
     Database.modules = [...Database.modules, newModule];
     return newModule;
   }
-  
-  export function deleteModule(moduleId) {
+export function deleteModule(moduleId) {
     const { modules } = Database;
     Database.modules = modules.filter((module) => module._id !== moduleId);
    }
-   
-   export function updateModule(moduleId, moduleUpdates) {
+export function updateModule(moduleId, moduleUpdates) {
     const { modules } = Database;
     const module = modules.find((module) => module._id === moduleId);
     Object.assign(module, moduleUpdates);
--- Kambaz/Modules/routes.js
+++ Kambaz/Modules/routes.js
@@ -1,5 +1,5 @@
 import * as modulesDao from "./dao.js";
-
+// I added async bc of an error
 export default function ModuleRoutes(app) {
     app.put("/api/modules/:moduleId", async (req, res) => {
         const { moduleId } = req.params;
@@ -8,9 +8,12 @@ export default function ModuleRoutes(app) {
         res.send(status);
       });
     
-    app.delete("/api/modules/:moduleId", async (req, res) => {
-        const { moduleId } = req.params;
-        const status = await modulesDao.deleteModule(moduleId);
-        res.send(status);
-    });
+ app.delete("/api/modules/:moduleId", async (req, res) => {
+   const { moduleId } = req.params;
+   const status = await modulesDao.deleteModule(moduleId);
+   res.send(status);
+   
+});
+
+
 }
