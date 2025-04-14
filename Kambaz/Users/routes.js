ed59e60f9ac14ff8f061c2904e028a08ac5f9c9c
ShireenKumar <shireen.kumar1304@gmail.com>
2025-04-14 17:50:32 -0400

    Finished until 6.3

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -67,34 +67,50 @@ export default function UserRoutes(app) {
   });
 
 
-  const createUser = async (req, res) => {
-    const newUser = await dao.createUser(req.body);
-    res.json(newUser);
-  };
-
-  const deleteUser = async (req, res) => {
-    const status = await dao.deleteUser(req.params.userId);
-    res.json(status);
-  };
-
   const findAllUsers = async (req, res) => {
+    const { role, name } = req.query;
+    if (role) {
+      const users = await dao.findUsersByRole(role);
+      res.json(users);
+      return;
+    }
+    if (name) {
+      const users = await dao.findUsersByPartialName(name);
+      res.json(users);
+      return;
+    }
     const users = await dao.findAllUsers();
     res.json(users);
   };
+
+  const updateUser = async (req, res) => {
+    const { userId } = req.params;
+    const userUpdates = req.body;
+    await dao.updateUser(userId, userUpdates);
+    const currentUser = req.session["currentUser"];
+   if (currentUser && currentUser._id === userId) {
+     req.session["currentUser"] = { ...currentUser, ...userUpdates };
+   }
+    res.json(currentUser);
+  };
+
+  const createUser = async (req, res) => {
+    const user = await dao.createUser(req.body);
+    res.json(user);
+  };
+
+
 
   const findUserById = async (req, res) => {
     const user = await dao.findUserById(req.params.userId);
     res.json(user);
   };
 
-  const updateUser = async (req, res) => {
-    const userId = req.params.userId;
-    const userUpdates = req.body;
-    await dao.updateUser(userId, userUpdates);
-    const currentUser = await dao.findUserById(userId);
-    req.session.currentUser = currentUser;
-    res.json(currentUser);
-  };
+  const deleteUser = async (req, res) => {
+    const status = await dao.deleteUser(req.params.userId);
+    res.json(status);
+};
+app.delete("/api/users/:userId", deleteUser);
 
 
   app.post("/api/users/signup", signup);
@@ -108,6 +124,6 @@ export default function UserRoutes(app) {
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
-  app.put("/api/users/:userId", updateUser);
   app.delete("/api/users/:userId", deleteUser);
+  app.put("/api/users/:userId", updateUser);
 }
